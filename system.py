fb886e928c1b1f5463a114c99971b7045b68db28
jasperkbos <jasperkbos@gmail.com>
2021-05-27 13:16:08 +0200

    Add system.py

---
+++ system.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Description ...
+# Authors: Jasper Bos (s3794687); Jim Casander (s3162753); Esther Ploeger (s3798461)
+# Date:    June 8th, 2021
+
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
