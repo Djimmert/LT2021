b1d318a3c85646da1e2a381fc5b9db17e970fa4b
jasperkbos <jasperkbos@gmail.com>
2021-05-27 14:13:36 +0200

    Implement falcon2.0

--- system.py
+++ system.py
@@ -4,9 +4,49 @@
 # Date:    June 8th, 2021
 
 
+import requests
+import csv
+
+
+def call_falcon(text):
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    url = 'https://labs.tib.eu/falcon/falcon2/api?mode=long'
+    entities = []
+    relations = []
+    payload = '{"text":"' + text + '"}'
+    r = requests.post(url, data=payload.encode('utf-8'), headers=headers)
+    if r.status_code == 200:
+        response = r.json()
+        for result in response['entities_wikidata']:
+            entities.append(result[0])
+            entities.append(result[1])
+        for result in response['relations_wikidata']:
+            relations.append(result[0])
+            relations.append(result[1])
+    else:
+        r = requests.post(url, data=payload.encode('utf-8'), headers=headers)
+        if r.status_code == 200:
+            response = r.json()
+            for result in response['entities_wikidata']:
+                entities.append(result[0])
+                entities.append(result[1])
+            for result in response['relations_wikidata']:
+                relations.append(result[0])
+                relations.append(result[1])
+
+    return entities, relations
+
+
 def main():
 
-
+    with open('test_questions.csv', 'r', encoding="utf-16") as f:
+        reader = csv.reader(f, delimiter='\t')
+        for row in reader:
+            print(row[1])
+            entities, relations = call_falcon(row[1])
+            print("Entities:", entities)
+            print("Properties:", relations)
+            print()
 
 
 if __name__ == "__main__":
