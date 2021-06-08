6b1f322bab68078061206c8aad4987194a0dd747
jasperkbos <jasperkbos@gmail.com>
2021-06-08 20:46:08 +0200

    Improve style

--- system_deps.py
+++ system_deps.py
@@ -15,7 +15,7 @@ def get_question_type(input_q):
     :param input_q: input question, plain text (str)
     :return: question type, abbreviation (str)
     """
-    
+
     # Define keywords
     duration_keywords = ['long', 'duration', 'minutes', 'time', 'length']
     time_keywords = ['century', 'year', 'when', 'month']
@@ -46,30 +46,30 @@ def get_question_type(input_q):
             elif any(item in duration_keywords for item in lemmas):
                 question_type = "duration"  # e.g. 'How long is X?'
             elif any(item in location_keywords for item in lemmas):
-                question_type = "location" # e.g. 'Where was X filmed?'
+                question_type = "location"  # e.g. 'Where was X filmed?'
             elif any(item in time_keywords for item in lemmas):
-                question_type = "time" # e.g. 'When was X published?'
+                question_type = "time"  # e.g. 'When was X published?'
             elif parse[0].text == "What" or parse[0].text == "Which":
                 if parse[1].pos_ == "NOUN":
                     if "VERB" in pos:
                         if "AUX" in pos and lemmas[pos.index("AUX")] == "be":
-                            question_type = "what_A_is_X_Y" # e.g 'What book is X based on?'
+                            question_type = "what_A_is_X_Y"  # e.g 'What book is X based on?'
                         elif "AUX" in pos and lemmas[pos.index("VERB")] == "earn":
-                            question_type = "what_A_is_X_Y" # e.g. 'Which movies earned X an award?'
+                            question_type = "what_A_is_X_Y"  # e.g. 'Which movies earned X an award?'
                         else:
-                            question_type = "what_which_verb" # e.g. 'What awards did X receive?'
+                            question_type = "what_which_verb"  # e.g. 'What awards did X receive?'
                     else:
-                        question_type = "whatXisY" # e.g. 'What genre is X?'
+                        question_type = "whatXisY"  # e.g. 'What genre is X?'
                 elif 'about' in lemmas:
                     question_type = "about"
                 else:
-                    question_type = "what_is_Xs_Y" # e.g. 'What is X's hair color?'
+                    question_type = "what_is_Xs_Y"  # e.g. 'What is X's hair color?'
             elif "tall" in lemmas:
-                question_type = "tall" # e.g 'How tall is X?'
+                question_type = "tall"  # e.g 'How tall is X?'
             elif ("many" in lemmas or "much" in lemmas) and "follower" not in lemmas and "cost" not in lemmas:
-                question_type = "count" # e.g. 'How many X films are there?'
+                question_type = "count"  # e.g. 'How many X films are there?'
             elif "cost" in lemmas:
-                question_type = "cost" # e.g. 'How much did X cost to make?'
+                question_type = "cost"  # e.g. 'How much did X cost to make?'
             else:
                 if 'pobj' in rel:
                     question_type = "XofY"  # e.g. 'Who is the director of X?'
@@ -157,9 +157,9 @@ def get_entity_property_deps(parse, question_type):
             if word.text.istitle() and word.i != 0:
                 entity_istitle.append((word.i, word.text))
         if len(entity_quotes) > 1:
-            ent = parse[entity_quotes[0][0]+1:entity_quotes[-1][0]].text.split(" ")
+            ent = parse[entity_quotes[0][0] + 1:entity_quotes[-1][0]].text.split(" ")
         elif entity_istitle:
-            ent = parse[entity_istitle[0][0]:entity_istitle[-1][0]+1].text.split(" ")
+            ent = parse[entity_istitle[0][0]:entity_istitle[-1][0] + 1].text.split(" ")
         else:
             ent = sent.split(" ")[dep.index("ROOT") + 1:]
 
@@ -181,9 +181,9 @@ def get_entity_property_deps(parse, question_type):
             if word.text.istitle() and word.i != 0:
                 entity_istitle.append((word.i, word.text))
         if len(entity_quotes) > 1:
-            ent = parse[entity_quotes[0][0]+1:entity_quotes[-1][0]].text.split(" ")
+            ent = parse[entity_quotes[0][0] + 1:entity_quotes[-1][0]].text.split(" ")
         elif entity_istitle:
-            ent = parse[entity_istitle[0][0]:entity_istitle[-1][0]+1].text.split(" ")
+            ent = parse[entity_istitle[0][0]:entity_istitle[-1][0] + 1].text.split(" ")
         else:
             ent = sent.split(" ")[dep.index("ROOT") + 1:]
         if 'from' in lemmas:
@@ -192,7 +192,7 @@ def get_entity_property_deps(parse, question_type):
             prop = {'P915': 'filming location'}
         elif 'born' in lemmas:
             prop = {'P19': 'place of birth'}
-        
+
     elif question_type == "time":
         entity_quotes = []
         entity_istitle = []
@@ -202,15 +202,15 @@ def get_entity_property_deps(parse, question_type):
             if word.text.istitle() and word.i != 0:
                 entity_istitle.append((word.i, word.text))
         if len(entity_quotes) > 1:
-            ent = parse[entity_quotes[0][0]+1:entity_quotes[-1][0]].text.split(" ")
+            ent = parse[entity_quotes[0][0] + 1:entity_quotes[-1][0]].text.split(" ")
         elif entity_istitle:
-            ent = parse[entity_istitle[0][0]:entity_istitle[-1][0]+1].text.split(" ")
+            ent = parse[entity_istitle[0][0]:entity_istitle[-1][0] + 1].text.split(" ")
         else:
             ent = sent.split(" ")[dep.index("ROOT") + 1:]
         if 'born' in sent or 'birthday' in sent:
             prop = {'P569': 'date of birth'}
-        elif 'release' in lemmas or 'come out' in sent or 'premiere' in sent\
-        or 'publish' in lemmas or 'publicise' in lemmas:
+        elif 'release' in lemmas or 'come out' in sent or 'premiere' in sent \
+                or 'publish' in lemmas or 'publicise' in lemmas:
             prop = {'P577': 'publication date'}
         elif 'die' in sent or 'pass away' in sent:
             prop = {'P570': 'date of death'}
@@ -225,12 +225,13 @@ def get_entity_property_deps(parse, question_type):
             if word.text.istitle() and word.i != 0:
                 entity_istitle.append((word.i, word.text))
         if len(entity_quotes) > 1:
-            ent = parse[entity_quotes[0][0]+1:entity_quotes[-1][0]].text.split(" ")
+            ent = parse[entity_quotes[0][0] + 1:entity_quotes[-1][0]].text.split(" ")
         elif entity_istitle:
-            ent = parse[entity_istitle[0][0]:entity_istitle[-1][0]+1].text.split(" ")
+            ent = parse[entity_istitle[0][0]:entity_istitle[-1][0] + 1].text.split(" ")
         else:
             ent = sent.split(" ")[dep.index("ROOT") + 1:]
-        if parse[-4:-2].text.split(" ") == ['based', 'on'] or parse[-4:-2].text.split(" ") == ['influenced', 'by'] or 'earned' in sent:
+        if parse[-4:-2].text.split(" ") == ['based', 'on'] or parse[-4:-2].text.split(" ") == ['influenced',
+                                                                                               'by'] or 'earned' in sent:
             prop = parse[-4:-2].text.split(" ")
         elif "AUX" in pos:
             prop = parse[1:pos.index("AUX")].text.split(" ")
@@ -255,7 +256,7 @@ def get_entity_property_deps(parse, question_type):
             for word in lemmas:
                 if word.istitle():
                     prop.append(word)
-       # Find property: probably last two words of sentence (parse[-4:-2])
+        # Find property: probably last two words of sentence (parse[-4:-2])
         prop = parse[-4:-2].text.split(" ")
 
     elif question_type == "tall":
@@ -267,7 +268,7 @@ def get_entity_property_deps(parse, question_type):
     elif question_type == "yes/no":
         main_verb_id = dep.index("ROOT")
         ent = lemmas[1:main_verb_id]
-        prop_broad = lemmas[main_verb_id+1:-2]
+        prop_broad = lemmas[main_verb_id + 1:-2]
         prop = [w for w in prop_broad if pos[lemmas.index(w)] != "DET"]
 
     else:
@@ -275,7 +276,7 @@ def get_entity_property_deps(parse, question_type):
 
     # Filter entity: starts with first capital letter and start is not an adjective (e.g. the Dutch movie ...)
     try:
-        start = min([ent.index(word) for word in ent if word.istitle() and \
+        start = min([ent.index(word) for word in ent if word.istitle() and
                      pos[sent.split(" ").index(word)] != "ADJ"])
         ent = ent[start:]
     except ValueError:
@@ -309,7 +310,7 @@ def retrieve_id_label(ent, prop):
     :param prop: property (str)
     :return: entity and property dictionaries with key: id and value: label
     """
-    
+
     url = 'https://www.wikidata.org/w/api.php'
 
     params_prop = {'action': 'wbsearchentities',
